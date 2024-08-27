a2be7401488da9affdf9a5529fb2866a4df1c81b
Mohammad Hamza <134505680+mohdhamzarehman@users.noreply.github.com>
2024-08-27 23:31:25 +0530

    Create Valid Anagram.java

---
+++ Valid Anagram.java
@@ -0,0 +1,19 @@
+HashMap<Character, Integer> hm = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            hm.put(ch, hm.getOrDefault(ch, 0) + 1);
+        }
+        for (int i = 0; i < t.length(); i++) {
+            char ch = t.charAt(i);
+            if (hm.get(ch) != null) {
+                if (hm.get(ch) == 1) {
+                    hm.remove(ch);
+                } else {
+                    hm.put(ch, hm.get(ch) - 1);
+                }
+            } else {
+                return false;
+            }
+
+        }
+        return hm.isEmpty();
